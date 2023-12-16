5995607dcacabf79cb5f187b3c38b96182445c31
rafay113 <rafaymyousuf.07@gmail.com>
2023-12-17 02:27:24 +0500

    all files

---
+++ javascript chap 8 tak/app.js
@@ -0,0 +1,106 @@
+        chap1
+
+// alert("Hello World");                  
+
+
+         chap2
+
+// var a = 5;
+// var b = 10;
+// var c = a+b;
+// alert(c)
+
+
+// // var a = 5;
+// // var b = 10;
+// // var c = a+b;
+// // console.log(c)
+
+
+// var b = 20; 
+// var d = "rafay";
+// var e = d+b;
+// alert(e)
+ 
+
+             chap3     
+               
+// var a = 3;
+// var b = 5;
+// var c = 7;
+// var d = (b - a) * c;
+// alert(d);
+
+
+// var a = 3;
+// var b = 5;
+// var c = 7;
+// var d = b - a  * c;
+// alert(d);
+
+
+
+
+// var a = 5;
+// var b = ++a;
+// alert(b)
+
+
+
+
+
+
+
+
+
+// var x = 10;
+// var y = x++ + ++x - x + x++ - ++x + x;
+//         // 10  + 12  -12 + 12  - 14  + 14 
+// alert(y)
+
+
+
+// var a = 7;
+// var b = a++ + ++a + a + --a + a-- - a
+//         // 7   +  9  + 9 + 8  +8 - 7
+                //   = 34 answer 
+// alert(b);          
+
+
+
+// var a = 4
+// var b = a++ + ++a + a + --a + a-- -a
+//         // 4   + 6   + 6 + 5   + 5 - 4
+//         //      = 22 answer      
+// alert(b);        
+
+
+// var a = 8
+// var b = a++ + ++a + a + --a + a-- - a  
+//         // 8   + 10  + 10 + 9  + 9 - 8
+//         //      = 38 answer
+// alert(b);  
+
+
+
+        //    moduleus ka question hai ?
+
+// var a = 10;
+// var b = 10 % 3
+// alert(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ javascript chap 9 sa/app.js
@@ -0,0 +1,190 @@
+                //  chap:- 9
+                //  prompt   
+               
+// var a = prompt("what is your number", "your num is" );
+// alert(a)
+
+
+                     //  console ma                   
+
+// var a = +prompt("what is your value","your value is" );
+// var b = a + 1000;
+// console.log("the output is",+""+b)
+
+                   //    alert ma 
+                  
+// var a = prompt("what is your value","your value is" );
+// var b = a + 5;
+// alert(b)
+
+
+            //   prompt ma ya tariqa chahe to istamal kar sakta hain.
+
+// var a = "what is your value";
+// var b = " your value";
+// var c = +prompt(a,b);
+// var d = c + 10;
+// alert(d);      
+
+
+//                  chap 10  
+//            if else conditions: 
+
+// var age = +prompt("enter your age");
+// if(age ==14){
+//     alert("your age is equal")
+// }
+// else if(age < 14){
+//      alert("your age is less")
+// } 
+// else{
+//     alert("your age is more")
+// }
+
+
+// var age = +prompt("Enter your age")
+// var gender = prompt("Enter your gender")
+// if(age > 18 && gender == "male"){
+//     alert("you are allowed to ride")
+// }
+// else{
+//     alert("you are not allowed")
+// } 
+
+
+// var a = "5";
+// if(a == 5){
+//     alert("true")
+// }
+// else{
+//     alert("false")
+// }
+
+
+
+                    // application mark sheet:-
+                              
+// var percent = +prompt("Enter your percent")
+// if(percent>= 80 && percent <= 100){
+//     alert("A+")
+// }
+// else if(percent >= 70 && percent <= 80){
+//         alert("A")
+// }
+// else if(percent >= 60 && percent <= 70){
+//     alert("B")
+// }
+// else if(percent >= 50 && percent <= 60){
+//     alert("C")
+// }
+// else if(percent >= 40 && percent <= 50){
+//     alert("D")
+// }
+// else if(percent >= 33 && percent <= 40){
+//     alert("E")
+// }
+// else if(percent >= 0 && percent <= 33){
+//     alert("F")
+// }
+// else{
+//     alert("you didnot write correct persentage")
+// }
+
+
+
+
+
+
+                    //  chap:-15                     
+                    //  Arrays:-
+
+
+// var fruits = ["apple","banana","graps"] 
+// alert(fruits[0])                    
+ 
+
+                   //      chap:-16
+                //          Arrays
+                // Adding and removing elements:-
+
+// var names = []
+
+// names[0] = "rafay"
+// names[1] = "sheikh"
+// alert( names)
+
+
+                   //      chap:-18
+                      //  For Loops:-
+// for(var i = 1; i <= 10; i++){
+//    document.write("5" + "x" + i + "=" + 5*i + "<br>")
+// }
+
+// for(var i = 0; i < 5; i++){
+//    alert("hello world!");
+// }
+
+// var names = ["rafay","sajid","rabeet"];
+ 
+// for(var i = 0; i< names.length; i++){
+//    console.log(names[i])
+// }
+
+// var firstnames = ["apple","banana","graps"]
+// var lastnames = ["mango"]
+
+// for(var i =0; i< firstnames.length; i++){
+//    for(var x =0; x< lastnames.length; x++){
+//       console.log(firstnames[i] + "" + lastnames[x])
+//    }
+// }
+
+// for(var a = 0; a < 55; a++){
+//    for(var i =0; i < a; i++){
+//     document.write("$")
+//    }
+//    document.write("<br>")
+     
+// }
+
+// var word = prompt("enter your words" , "my words is" ,  );
+// var check = "";
+// for(var i = word.length - 1; i >= 0; i--){
+//    check += [i]
+// }
+// document.write(word + " is palindrome words?");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
